4283e5229ea913e792f600068212925757ad5990
Xin Li <hcmhcm426@gmail.com>
2018-09-27 08:06:44 +0000

    yelpcamp1

---
+++ APIs/movie_search_app/app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+   res.render("search");
+});
+
+app.get("/result", function(req, res){
+    var query = req.query.search;
+    var url = "http://omdbapi.com/?s=" + query;
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200) {
+            var data = JSON.parse(body)
+            res.render("result", {data: data});
+        }
+    });
+});
